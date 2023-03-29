44ba03dedb485bbd116f11e1d80bde890ca3e82c
mockingb <m.1@bk.ru>
2023-03-29 18:24:06 +0300

    6

--- S21Gtest.cc
+++ S21Gtest.cc
@@ -1,26 +1,32 @@
-#include <iostream>
-#include "s21_list.h"
 #include <gtest/gtest.h>
+
+#include <iostream>
+
+#include "s21_list.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
 using namespace s21;
 
-TEST(Constructors, default) {
-    List<int> a;
+TEST(Constructors, default) { List<int> a; }
+TEST(Constructors, size_n) {
+  List<int> a(10);
+  EXPECT_EQ(a.size(), 10);
+}
+TEST(Constructors, list) {
+  List<int> a({1, 2, 3, 4, 5});
+  EXPECT_EQ(a.size(), 5);
 }
 
 TEST(Methods, push_back) {
-  
-    List<int> a;
-    a.push_back(3);
-    EXPECT_EQ(a.size(), 1);
-    a.push_back(5);
-    EXPECT_EQ(a.back(), 5);
-    
-    EXPECT_EQ(a.front(), 3);
-    EXPECT_EQ(a.size(), 2);
+  List<int> a;
+  a.push_back(3);
+  EXPECT_EQ(a.size(), 1);
+  a.push_back(5);
+  EXPECT_EQ(a.back(), 5);
+  EXPECT_EQ(a.front(), 3);
+  EXPECT_EQ(a.size(), 2);
 }
 TEST(Methods, pop_back) {
   List<double> d;
@@ -29,10 +35,33 @@ TEST(Methods, pop_back) {
   EXPECT_EQ(d.size(), 2);
   d.pop_back();
   EXPECT_EQ(d.size(), 1);
+  d.pop_back();
+  EXPECT_EQ(d.size(), 0);
+  EXPECT_NO_THROW(d.pop_back());
+}
+TEST(Methods, push_front) {
+  List<int> i;
+  i.push_front(100);
+  EXPECT_EQ(i.front(), 100);
+  EXPECT_EQ(i.size(), 1);
+  i.push_front(200);
+  EXPECT_EQ(i.front(), 200);
+  EXPECT_EQ(i.back(), 100);
+  EXPECT_EQ(i.size(), 2);
+}
+TEST(Methods, pop_front) {
+  List<double> d;
+  d.push_back(2.3);
+  d.push_back(3.4);
+  EXPECT_EQ(d.size(), 2);
+  d.pop_front();
+  EXPECT_EQ(d.size(), 1);
+  d.pop_front();
+  EXPECT_EQ(d.size(), 0);
+  EXPECT_NO_THROW(d.pop_front());
 }
 
 int main(int argc, char* argv[]) {
-    ::testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-    
 }
--- s21_list.h
+++ s21_list.h
@@ -5,90 +5,122 @@ namespace s21 {
 
 template <class T>
 class List {
- struct node{
-   T val_;
-   node *next, *prev;
-   node() {
-     val_ = 0;
-     next = prev = 0;
-   }
-   node(T val) {
-     val_ = val;
-     next = prev = 0;
-   }
- };
- node *base, *head, *tail;
-//  size_t size;
-
  public:
+  using value_type = T;
+  using reference = T&;
+  using const_reference = const T&;
+  using iterator = T*;
+  using const_iterator = const T*;
+  using size_type = size_t;
 
- // constuctors
- List(){
-   try
-   {
-     base = new node();
-   }
-   catch(const std::exception& e)
-   {
-     std::cerr << e.what() << '\n';
-   }
-   
-   base->prev=base->next;
-   head = tail = base;
- }
- ~List() {
-   base->next = 0;
-          node *tmp = 0;
-   while (head != 0)
-   {
-          tmp = head->next;
-          delete head;
-          head = tmp;
-   }
-   
- }
+  // constuctors
+  List() {
+    try {
+      base_ = new node();
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
+    }
+    base_->prev_ = base_->next_;
+    head_ = tail_ = base_;
+  }
+  List(size_type n) : List() {
+    while (n--) {
+      push_back(0);
+    }
+  }
+  List(std::initializer_list<value_type> const& items) : List() {
+    for (auto it : items) push_back(it);
+  }
+  List(const List &l) : List() {
+    
+  }
+  ~List() {
+    base_->next_ = 0;
+    node* tmp = 0;
+    while (head_ != 0) {
+      tmp = head_->next_;
+      delete head_;
+      head_ = tmp;
+    }
+  }
 
+  // methods
+  const_reference front() { return head_->val_; };
+  const_reference back() { return tail_->val_; };
+  void push_back(value_type val) {
+    node* tmp = 0;
+    try {
+      tmp = new node(val);
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
+      return;
+    }
+    if (head_ == base_) head_ = tmp;
+    tail_->next_ = tmp;
+    tmp->prev_ = tail_;
+    tmp->next_ = base_;
+    tail_ = tmp;
+  };
+  void pop_back() {
+    if (head_ == base_) return;
+    node* tmp = tail_;
+    if (tail_->prev_ == base_) {
+      head_ = tail_ = base_;
+    } else {
+      tail_ = tail_->prev_;
+      tail_->next_ = base_;
+    }
+    delete tmp;
+  };
+  void push_front(value_type val) {
+    node* tmp = 0;
+    try {
+      tmp = new node(val);
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
+      return;
+    }
+    if (head_ == base_) {
+      tail_ = tmp;
+      tmp->next_ = base_;
+    } else {
+      tmp->next_ = head_;
+    }
+    base_->next_ = tmp;
+    tmp->prev_ = base_;
+    head_ = tmp;
+  };
+  void pop_front() {
+    if (head_ == base_) return;
+    node* tmp = head_;
+    if (head_->next_ == base_) {
+      head_ = tail_ = base_;
+    } else {
+      base_->next_ = head_->next_;
+      head_ = head_->next_;
+      head_->prev_ = base_;
+    }
+    delete tmp;
+  };
+  size_t size() {
+    int count{};
+    for (node* n = head_; n != base_; n = n->next_) count++;
+    return count;
+  }
 
-   // methods
-   T front() {
-     return head->val_;
-   };
-   T back() {
-     return tail->val_;
-   };
-   void push_back(T val) {
-     node *tmp = 0;
-     try
-     {
-       tmp = new node(val);
-     }
-     catch(const std::exception& e)
-     {
-       std::cerr << e.what() << '\n';
-     }
-     
-     if(head == base) head = tmp;
-     tail->next = tmp;
-     tmp->prev = tail;
-     tmp->next = base;
-     tail = tmp;
-    //  size++;
-   };
-   void pop_back() {
-     if(head==base) return;
-     node *tmp = tail;
-     tail = tail->prev;
-     tail->next = base;
-     delete tmp;
-   };
-   void push_front() {};
-   void pop_front() {};
-   size_t size() {
-     int count{};
-     for(node* n = head; n != base; n = n->next)
-     count++;
-     return count;
-   }
+  struct node {
+    value_type val_;
+    node *next_, *prev_;
+    node() {
+      val_ = 0;
+      next_ = prev_ = 0;
+    }
+    node(value_type val) {
+      val_ = val;
+      next_ = prev_ = 0;
+    }
+  };
+  node *base_, *head_, *tail_;
 };
 };      // namespace s21
 #endif  // S21_LIST_H_
